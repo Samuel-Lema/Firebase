8fea6dfa861ccc2a9863db1fd1c6e4d7ccbcdaad
samuel-lema <thundersoulgamer@gmail.com>
2019-03-26 11:32:20 +0100

    Solucionado problema con las notificaciones push.

--- app/src/main/java/com/gamemotion/mydatabase/MessagingService.kt
+++ app/src/main/java/com/gamemotion/mydatabase/MessagingService.kt
@@ -1,32 +1,30 @@
 package com.gamemotion.mydatabase
 
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.media.RingtoneManager
-import android.support.v4.app.NotificationCompat
+import android.annotation.SuppressLint
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import android.app.NotificationManager
+import android.media.RingtoneManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.support.v4.app.NotificationCompat
 
-class MyFirebaseMessagingService : FirebaseMessagingService() {
-    val TAG = "Servicio"
 
-    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
-        // Handle FCM messages here.
-        // If the application is in the foreground handle both data and notification messages here.
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated.
-        Log.d(TAG, "De: " + remoteMessage!!.from)
-        Log.d(TAG, "Mensaje: " + remoteMessage.notification!!.body!!)
+class MessagingService : FirebaseMessagingService() {
+
+    @SuppressLint("LongLogTag")
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d("Notificacion", "Dikirim dari: ${remoteMessage.from}")
+
         if (remoteMessage.notification != null) {
             showNotification(remoteMessage.notification?.title, remoteMessage.notification?.body)
         }
     }
 
     private fun showNotification(title: String?, body: String?) {
-        val intent = Intent(this, UserMain::class.java)
+        val intent = Intent(this, ChatZone::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(this, 0, intent,
             PendingIntent.FLAG_ONE_SHOT)
